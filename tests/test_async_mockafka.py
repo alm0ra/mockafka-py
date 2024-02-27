e2522709ac921e75153c7162498f8d7e6e395845
ali <ali.mrd318@gmail.com>
2024-02-27 21:13:09 +0330

    add more test case for testing decorators add pytest-asyncio package

---
+++ tests/test_async_mockafka.py
@@ -0,0 +1,67 @@
+import pytest
+from aiokafka.admin import NewTopic
+
+from mockafka import aproduce, asetup_kafka, aconsume
+from mockafka.aiokafka import FakeAIOKafkaConsumer, FakeAIOKafkaProducer, FakeAIOKafkaAdmin
+
+
+# Here is a reference snippet of code from mockafka/aiokafka/aiokafka_admin.py:
+@pytest.mark.asyncio
+async def test_create_topics():
+    admin_client = FakeAIOKafkaAdmin()
+    await admin_client.create_topics(new_topics=[
+        NewTopic(name='test_topic', num_partitions=1, replication_factor=1),
+    ])
+
+
+# Here is a reference snippet of code from fake aiokafka producer consumer & admin client:
+@pytest.mark.asyncio
+async def test_produce_and_consume():
+    # create topic via admin client
+    admin_client = FakeAIOKafkaAdmin(clean=True)
+    await admin_client.create_topics(new_topics=[
+        NewTopic(name='test_topic', num_partitions=1, replication_factor=1),
+    ])
+
+    # produce message via producer client
+    producer = FakeAIOKafkaProducer()
+    await producer.start()
+    await producer.send(headers={}, key='test_key', value='test_value', topic='test_topic', partition=0, )
+
+    # consume message via consumer client
+    consumer = FakeAIOKafkaConsumer()
+    await consumer.start()
+    consumer.subscribe(['test_topic'])
+    message = await consumer.getone()
+
+    assert message.key() == 'test_key'
+    assert message.value() == 'test_value'
+
+    message = await consumer.getone()
+
+    assert message is None
+
+
+@pytest.mark.asyncio
+@asetup_kafka(topics=[{'topic': 'test_topic', 'partition': 16}], clean=True)
+@aproduce(topic='test_topic', value='test_value', key='test_key', partition=0)
+async def test_produce_with_decorator():
+    consumer = FakeAIOKafkaConsumer()
+    await consumer.start()
+    consumer.subscribe(['test_topic'])
+    message = await consumer.getone()
+
+    assert message.key() == 'test_key'
+    assert message.value() == 'test_value'
+
+
+@pytest.mark.asyncio
+@asetup_kafka(topics=[{'topic': 'test_topic', 'partition': 16}], clean=True)
+@aproduce(topic='test_topic', value='test_value', key='test_key', partition=0)
+@aconsume(topics=['test_topic'])
+async def test_produce_and_consume_with_decorator(message=None):
+    if not message:
+        return
+
+    assert message.key() == 'test_key'
+    assert message.value() == 'test_value'
